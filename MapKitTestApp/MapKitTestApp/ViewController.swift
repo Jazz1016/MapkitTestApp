71fcab45fe678482937a379c5b4291d5f13c038b
James L <59751464+Jazz1016@users.noreply.github.com>
2021-08-05 17:09:29 -0600

    test app working

--- MapKitTestApp/MapKitTestApp/ViewController.swift
+++ MapKitTestApp/MapKitTestApp/ViewController.swift
@@ -5,15 +5,45 @@
 //  Created by James Lea on 8/4/21.
 //
 
+import CoreLocation
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let map: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(map)
+        title = "Home"
+        
+        LocationManager.shared.getUserLocation { [weak self] location in
+            DispatchQueue.main.async {
+                guard let strongSelf = self else {return}
+                strongSelf.addMapPin(with: location)
+            }
+        }
     }
-
-
-}
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        map.frame = view.bounds
+    }
+    
+    func addMapPin(with location: CLLocation){
+        let pin = MKPointAnnotation()
+        pin.coordinate = location.coordinate
+        map.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.7, longitudeDelta: 0.7)), animated: true)
+        map.addAnnotation(pin)
+        
+        LocationManager.shared.resolveLocationName(with: location) { [weak self] locationName in
+            self?.title = locationName
+        }
+    }
+    
+}//End of class
 
